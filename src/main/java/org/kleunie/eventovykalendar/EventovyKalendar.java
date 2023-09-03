239149903de6e839a90e58f2ddce2c25db544a1d
Kleuska <143964779+kleuska@users.noreply.github.com>
2023-09-03 22:42:13 +0200

    1.20.1

---
+++ src/main/java/org/kleunie/eventovykalendar/EventovyKalendar.java
@@ -0,0 +1,17 @@
+package org.kleunie.eventovykalendar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventovyKalendar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
